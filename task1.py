4325ea8d4b9f367542ef7d9539d0b3c4c027fd3c
it_zmatt <mohamed.taous@univ-khenchela.dz>
2023-06-02 13:47:42 +0200

    9th task Done: Add colors to a scatter

--- task1.py
+++ task1.py
@@ -96,3 +96,19 @@ for lab, row in netflix_movies_col_subset.iterrows ():
 # Inspect the first 10 values in your list        
 print(colors[0:10])
 
+# Task 9
+
+# Set the figure style and initalize a new figure
+plt.style.use('fivethirtyeight')
+fig = plt.figure(figsize=(12,8))
+
+# Create a scatter plot of duration versus release_year
+plt.scatter(netflix_movies_col_subset[['release_year']], netflix_movies_col_subset[['duration']], c = colors)
+
+# Create a title and axis labels
+plt.title('Movie duration by year of release')
+plt.xlabel('Release year')
+plt.ylabel('Duration (min)')
+
+# Show the plot
+plt.show()
